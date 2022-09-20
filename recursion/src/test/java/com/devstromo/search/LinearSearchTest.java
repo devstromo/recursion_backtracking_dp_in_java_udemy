4a641d786ac6d6a15e24ec9f58da657614e0f334
alemoles <alemolerio@gmail.com>
2022-09-19 23:38:39 -0300

    code refactor

--- recursion/src/test/java/com/devstromo/search/LinearSearchTest.java
+++ recursion/src/test/java/com/devstromo/search/LinearSearchTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.devstromo.recursion.Fibonacci;
-
 class LinearSearchTest {
 
     private static LinearSearch search;
